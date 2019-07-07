1c265d60384598bcc45f3b804d1778c053cfb486
kwanghyunMoon <mgh3326@naver.com>
2019-07-07 12:23:20 +0900

    first update

---
+++ src/main/java/me/khmoon/reservationmainpage/ReservationMainPageApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.reservationmainpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationMainPageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReservationMainPageApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/khmoon/reservationmainpage/ReservationMainPageApplicationTests.java
@@ -0,0 +1,16 @@
+package me.khmoon.reservationmainpage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReservationMainPageApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
